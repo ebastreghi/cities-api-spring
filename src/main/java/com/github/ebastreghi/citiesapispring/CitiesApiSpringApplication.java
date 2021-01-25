58869eccb2eb9bb7a9323e8148c49443b334830b
Edevar Bastreghi Filho <edevarfilho@gmail.com>
2021-01-25 07:13:47 -0300

    Config DB
    Initial springboot project, config DB

---
+++ src/main/java/com/github/ebastreghi/citiesapispring/CitiesApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.github.ebastreghi.citiesapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ebastreghi/citiesapispring/CitiesApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ebastreghi.citiesapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
